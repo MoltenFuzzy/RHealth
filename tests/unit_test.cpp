83e95bf2519b851d3d4bc179ab6bd09f20d333e5
Moltenfuzzy <kentphan2000@gmail.com>
2020-11-22 22:54:50 -0800

    Create gtest test

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -1,5 +1,9 @@
 #include "gtest/gtest.h"
 
+TEST(test1, ex1) {
+  EXPECT_EQ("hello", "hello");
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
