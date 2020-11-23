2843e8c97a83e60f692db46bf47978d5c7acaa25
AndreaZaragoza <azara023@ucr.edu>
2020-11-22 19:48:13 -0800

    Component: HealthPlan

--- src/HealthPlan.hpp
+++ src/HealthPlan.hpp
@@ -1,12 +1,25 @@
+#ifndef _HEALTHPLAN_HPP_
+#define _HEALTHPLAN_HPP_
 
+#include <string>
 
-class HealthPlan
-{
-private:
-	/* data */
-public:
-	HealthPlan(/* args */);
-	~HealthPlan();
+class HealthPlan {
+	private:
+		/* data */
+		int age;
+		string sex;
+		double weight;
+		double height;
+
+	public:
+		/* Constructor(s) */
+		HealthPlan() { };
+	
+		/* Destructor */
+		~HealthPlan();
+		
+		/* Functions */
+		virtual void Accept(HealthPlanVisitor);
 };
 
-
+#endif
