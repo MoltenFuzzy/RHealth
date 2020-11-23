43b0cdd8c6191d7e6b5ecb607339fe75f81b460c
supersydney22 <sgarc119@ucr.edu>
2020-11-23 10:33:59 -0800

    test files updated but not functional

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -1,6 +1,24 @@
+#ifndef PROJECT_TESTS_HPP
+#define PROJECT_TESTS_HPP
+
 #include "gtest/gtest.h"
+#include "../src/WeightTracker.hpp"
+#include "../src/HealthPlan.hpp"
+
+TEST(bmi, normalTest) {
+        double height = 5.3;
+        double weight = 129.0;
+        WeightTracker *bmi = new WeightTracker();
+	bmi->set_height(height);
+	bmi->set_weight(weight);
+
+        EXPECT_EQ(bmi->BmiCalculator(height, weight), 23);
+}
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+
+#endif //PROJECT_TESTS_HPP
