458034299136cc3bb418f00ef31be90bfff7940f
Moltenfuzzy <kentphan2000@gmail.com>
2020-11-26 14:34:39 -0800

    Add unit tests

--- tests/ExercisePlanTest.cpp
+++ tests/ExercisePlanTest.cpp
@@ -8,32 +8,137 @@
 
 //TODO: create more unit tests
 
-// TEST(ExercisePlanTest, AddElementLeafTest) {
-// 	Workout* push_ups = new Workout("Push ups");
-// 	Workout* sit_ups = new Workout("Sit ups");
-// 	Workout* ten_k_run = new Workout("10k run");
+TEST(ExercisePlanTest, AddElementLeafTest) {
+	std::string workout1 = "Push Ups";
+	std::string workout2 = "Sit Ups";
+	std::string workout3 = "10k Run";
 
-// 	DailyExercise* test_plan = new DailyExercise(); 
-//     test_plan->Add(push_ups);
-//     ASSERT_EQ(test_plan->size(), 1);
-//     test_plan->Add(sit_ups);
-// 	test_plan->Add(ten_k_run);
-// 	EXPECT_EQ(test_plan->GetDailyExercises().at(0)->GetWorkoutName(), "Push ups");
-// 	EXPECT_EQ(test_plan->GetDailyExercises().at(1)->GetWorkoutName(), "Sit ups");
-// 	EXPECT_EQ(test_plan->GetDailyExercises().at(2)->GetWorkoutName(), "10k run");
-// }
+	Workout* PushUps = new Workout(workout1);
+	Workout* SitUps = new Workout(workout2);
+	Workout* TenK_run = new Workout(workout3);
 
-// TEST(ExercisePlanTest, AddElementTreeTest) {
-// 	Workout* push_ups = new Workout("Push ups");
-// 	Workout* sit_ups = new Workout("Sit ups");
-// 	Workout* ten_k_run = new Workout("10k run");
+	Routine* test_plan = new Routine(); 
 
-// 	ExercisePlan* test_plan = new ExercisePlan(); 
-//     test_plan->Add(push_ups);
-//     ASSERT_EQ(test_plan->size(), 1);
-//     test_plan->Add(sit_ups);
-// 	test_plan->Add(ten_k_run);
-// 	EXPECT_EQ(test_plan->GetWeeklyExercises().at(0)., "Push ups");
-// }
+    test_plan->Add(workout1, PushUps);
+    ASSERT_EQ(test_plan->Size(), 1);
+    test_plan->Add(workout2, SitUps);
+	test_plan->Add(workout3, TenK_run);
+	EXPECT_EQ(test_plan->GetRoutines().at(0).first, workout1);
+	EXPECT_EQ(test_plan->GetRoutines().at(1).first, workout2);
+	EXPECT_EQ(test_plan->GetRoutines().at(2).first, workout3);
+}
+
+TEST(ExercisePlanTest, AddTreeTest) {
+	std::string workout1 = "Push Ups";
+	std::string workout2 = "Sit Ups";
+	std::string workout3 = "10k Run";
+
+	Workout* PushUps = new Workout(workout1);
+	Workout* SitUps = new Workout(workout2);
+	Workout* TenK_run = new Workout(workout3);
+
+	Routine* MondayPlan = new Routine();
+	Routine* TuesdayPlan = new Routine();
+
+	Routine* WeeklyPlanTest = new Routine(); 
+
+    MondayPlan->Add(workout1, PushUps);
+    ASSERT_EQ(MondayPlan->Size(), 1);
+    MondayPlan->Add(workout2, SitUps);
+	MondayPlan->Add(workout3, TenK_run);
+
+	TuesdayPlan->Add(workout1, PushUps);
+    ASSERT_EQ(TuesdayPlan->Size(), 1);
+    TuesdayPlan->Add(workout2, SitUps);
+	TuesdayPlan->Add(workout3, TenK_run);
+
+	std::string day1 = "monday";
+	std::string day2 = "tuesday";
+
+	WeeklyPlanTest->Add(day1, MondayPlan);
+    ASSERT_EQ(WeeklyPlanTest->Size(), 1);
+    WeeklyPlanTest->Add(day2, TuesdayPlan);
+
+	EXPECT_EQ(WeeklyPlanTest->GetRoutines().at(0).first, day1);
+	EXPECT_EQ(WeeklyPlanTest->GetRoutines().at(1).first, day2);
+
+	EXPECT_EQ(WeeklyPlanTest->GetRoutines().at(0).second, MondayPlan);
+	EXPECT_EQ(WeeklyPlanTest->GetRoutines().at(1).second, TuesdayPlan);
+}
+
+TEST(ExercisePlanTest, RemoveElementTest) {
+	std::string workout1 = "Push Ups";
+	std::string workout2 = "Sit Ups";
+	std::string workout3 = "10k Run";
+
+	Workout* PushUps = new Workout(workout1);
+	Workout* SitUps = new Workout(workout2);
+	Workout* TenK_run = new Workout(workout3);
+
+	Routine* test_plan = new Routine(); 
+
+	// Adding
+    test_plan->Add(workout1, PushUps);
+    ASSERT_EQ(test_plan->Size(), 1);
+    test_plan->Add(workout2, SitUps);
+	test_plan->Add(workout3, TenK_run);
+	EXPECT_EQ(test_plan->GetRoutines().at(0).first, workout1);
+	EXPECT_EQ(test_plan->GetRoutines().at(1).first, workout2);
+	EXPECT_EQ(test_plan->GetRoutines().at(2).first, workout3);
+
+	// Removing 
+	test_plan->Remove(workout1, PushUps);
+    test_plan->Remove(workout2, SitUps);
+	test_plan->Remove(workout3, TenK_run);
+
+	EXPECT_EQ(test_plan->Size(), 0);
+}
+
+TEST(ExercisePlanTest, RemoveTreeTest) {
+	std::string workout1 = "Push Ups";
+	std::string workout2 = "Sit Ups";
+	std::string workout3 = "10k Run";
+
+	Workout* PushUps = new Workout(workout1);
+	Workout* SitUps = new Workout(workout2);
+	Workout* TenK_run = new Workout(workout3);
+
+	Routine* MondayPlan = new Routine();
+	Routine* TuesdayPlan = new Routine();
+
+	Routine* WeeklyPlanTest = new Routine(); 
+
+	// Adding
+    MondayPlan->Add(workout1, PushUps);
+    ASSERT_EQ(MondayPlan->Size(), 1);
+    MondayPlan->Add(workout2, SitUps);
+	MondayPlan->Add(workout3, TenK_run);
+
+	TuesdayPlan->Add(workout1, PushUps);
+    ASSERT_EQ(TuesdayPlan->Size(), 1);
+    TuesdayPlan->Add(workout2, SitUps);
+	TuesdayPlan->Add(workout3, TenK_run);
+
+	std::string day1 = "monday";
+	std::string day2 = "tuesday";
+
+	WeeklyPlanTest->Add(day1, MondayPlan);
+    ASSERT_EQ(WeeklyPlanTest->Size(), 1);
+    WeeklyPlanTest->Add(day2, TuesdayPlan);
+
+	EXPECT_EQ(WeeklyPlanTest->GetRoutines().at(0).first, day1);
+	EXPECT_EQ(WeeklyPlanTest->GetRoutines().at(1).first, day2);
+
+	EXPECT_EQ(WeeklyPlanTest->GetRoutines().at(0).second, MondayPlan);
+	EXPECT_EQ(WeeklyPlanTest->GetRoutines().at(1).second, TuesdayPlan);
+
+	// Removing
+	WeeklyPlanTest->Remove(day1, MondayPlan);
+    WeeklyPlanTest->Remove(day2, TuesdayPlan);
+
+	EXPECT_EQ(WeeklyPlanTest->Size(), 0);
+
+}
+
 
 
