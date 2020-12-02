5a6d27628d70fa972c7ced3c3264dc069d1eed84
supersydney22 <sgarc119@ucr.edu>
2020-12-02 14:37:39 -0800

    Removed files

---
+++ CMakeFiles/Rhealth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rhealth.dir/src/main.cpp.o"
+  "Rhealth"
+  "Rhealth.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rhealth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
