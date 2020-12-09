fca57a12720530a6a68ea8a771a838bc9d6ab7ab
Moltenfuzzy <kentphan2000@gmail.com>
2020-12-08 22:02:15 -0800

    Update Cmakelists and fix header guard

--- src/HealthPlan.hpp
+++ src/HealthPlan.hpp
@@ -7,20 +7,20 @@
 
 #include "APIHandler.hpp"
 
-class APIHandler; 
+class APIHandler;
 
-class HealthPlan {
+class HealthPlan
+{
 private:
+protected:
+	int age = 0;
+	std::string sex;
+	double height = 0;
+	double weight = 0;
 
-protected: 
-	int age = 0; 
-	std::string sex; 
-	double height = 0; 
-	double weight = 0; 
+	double BMI = 0;
 
-	double BMI = 0; 
-
-	APIHandler* APIFunction = nullptr; 
+	APIHandler *APIFunction = nullptr;
 	std::vector<std::string> DaysOfWeek = {"sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"};
 
 	std::string API_url = "";
@@ -32,26 +32,23 @@ public:
 	virtual void Add() {}
 	virtual void Remove() {}
 	virtual void Print() {}
-	virtual std::string getAPIurl() = 0; 
-	virtual std::string getAPItoken() = 0; 
+	virtual std::string getAPIurl() = 0;
+	virtual std::string getAPItoken() = 0;
 
 	double getBMI() { return this->BMI; }
 
-	void setAPIFunction(APIHandler* function) { this->APIFunction = function; }
+	void setAPIFunction(APIHandler *function) { this->APIFunction = function; }
 
-	double CalcBMI(double weight, double height) { 
-		BMI = weight / pow(height/100, 2); 
-		return BMI; 
+	double CalcBMI(double weight, double height)
+	{
+		BMI = weight / pow(height / 100, 2);
+		return BMI;
 	}
 
 	bool IsUnderWeight(double BMI) { return BMI <= 18.5; }
 	bool IsNormalWeight(double BMI) { return 18.5 < BMI && BMI < 24.9; }
 	bool IsOverWeight(double BMI) { return 25.0 < BMI && BMI < 29.9; }
 	bool IsObese(double BMI) { return BMI >= 30.0; }
-
 };
-
-#endif
-
 
 #endif // HEALTH_PLAN
