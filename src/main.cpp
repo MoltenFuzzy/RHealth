a3c70dd16467f9a4a3da184a8976308f803b5d83
Moltenfuzzy <kentphan2000@gmail.com>
2020-12-10 05:09:50 -0800

    Fix formatting

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,15 @@ int main()
 	HealthApp RHealth;
 
 	// Testing stuff
-	HealthPlan *Exercise_Plan = RHealth.CreateExercisePlan(20, "Male", 80, 180);
+	// HealthPlan *Exercise_Plan = RHealth.CreateExercisePlan(20, "Male", 80, 180);
 
-	ExercisePlan *test = dynamic_cast<ExercisePlan *>(Exercise_Plan);
+	// ExercisePlan *test = dynamic_cast<ExercisePlan *>(Exercise_Plan)->Search("Back Squat");
 
-	test->Print(std::cout);
-	test->Search("Calf Raises");
-	std::cout << test->GetDesc();
+	// test->Print(std::cout);
 
-	// RHealth.Run();
+	// std::cout << test->GetDesc();
+
+	RHealth.Run();
 
 	return 0;
 }
