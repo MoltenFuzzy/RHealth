d426cea5b85ea1bfafe00b4fc9ecf4081ae1d8aa
supersydney22 <sgarc119@ucr.edu>
2020-12-09 21:07:54 -0800

    Weight Tracker test file

--- tests/projectTests.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FINAL_PROJECTTESTS_HPP
-#define FINAL_PROJECTTESTS_HPP
-
-#include "gtest/gtest.h"
-
-//file can be deleted :)
-
-
-#endif //FINAL_PROJECTTESTS_HPP
--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -3,6 +3,7 @@
 
 #include "gtest/gtest.h"
 #include "ExercisePlanTest.cpp"
+#include "WeightTrackerTest.cpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
