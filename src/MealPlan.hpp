ac04aaa6f6236d5f605b2f456dc825bdf0247772
AndreaZaragoza <azara023@ucr.edu>
2020-12-11 00:30:06 -0800

    MealPlan Composite Update for Meal and Meals inheritance

--- src/MealPlan.hpp
+++ src/MealPlan.hpp
@@ -4,7 +4,6 @@
 // Files
 #include "HealthPlan.hpp"
 #include "APICaller.hpp"
-#include "Meals.hpp"
 // User Libraries
 #include <string>
 #include <vector>
@@ -19,31 +18,41 @@ using namespace curlpp::options;
 class MealPlan:virtual public HealthPlan{
 	protected:
 		json MealData;
-		std::string meal_url="http://www.melskitchencafe.com/the-best-fudgy-brownies/";
+		std::string meal_url="https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=healthy&diet=vegetarian&excludeIngredients=coconut&intolerances=egg%2C%20gluten&number=21&offset=0&type=main%20course";
 		
 	public:
 		MealPlan(int age, std::string sex, double weight, double height){}
 
 		void MealPlan(){
-			setAPItoken("f6bcf37fdbmsh1c2a258eab8c8a6p1e59ddjsn1d8341acc7f8");
-			AddToAPIheader("f6bcf37fdbmsh1c2a258eab8c8a6p1e59ddjsn1d8341acc7f8");
-			AddToAPIheader("spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+			setAPItoken("x-rapidapi-key: 9db5831fb1mshc6cc5560f899fadp151893jsn29ec635b5745");
+			AddToAPIheader("x-rapidapi-key: 9db5831fb1mshc6cc5560f899fadp151893jsn29ec635b5745");
+			AddToAPIheader("x-rapidapi-host: spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
 		}
 		
-		virtual void Add(MealPlan * recipe){}
+		virtual void Add(std::string MealName,MealPlan * MealLink){}
 
-		virtual void Remove(MealPlan * recipe){}
+		virtual void Remove(std::string MealName,MealPlan * MealLink){}
 
-		MealPlan GetChild(){}
+		//MealPlan GetChild(){}
 
-		SetAPI(APIHandler* AnAPI) {}
+		//SetAPI(APIHandler* AnAPI) {}
+
+		virtual void PrintMeal(std::ostream &outs)=0;
+
+		virtual std::string GetMealName(){}
+
+		virtual std::string GetMealLink(){}
+
+		void SetMealData(json m){this->MealData=m;}
+
+		json GetMealData(){return this->MealData;}
 
 		const json &CallAPI(){
 			if(APIFunction==nullptr){
 				throw std::runtime_error("Invalid API");
 			}
 			else{
-				this->payload=APIFunction->CallAPI(this)["RecipeResults"];
+				this->payload=APIFunction->CallAPI(this)["results"];
 			}
 			return this->payload;
 		} 
