1e7a7ca476ecfcfbedbfe3423e1121f87a6a553c
supersydney22 <sgarc119@ucr.edu>
2020-12-11 10:46:46 -0800

    bmi test

--- tests/WeightTrackerTest.cpp
+++ tests/WeightTrackerTest.cpp
@@ -2,6 +2,14 @@
 
 #include "../src/WeightTracker.hpp"
 
+TEST(CalcBMI, RegularValues)
+{
+	double weight = 58.95;
+	double height = 5.25;
+	HealthPlan* bmi = new HealthPlan();
+
+	EXPECT_EQ(bmi->CalcBMI(height, weight), 23);
+}
 
 
 
